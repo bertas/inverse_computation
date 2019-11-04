5a3bbacf97f1cf5ab0fb8783ed5ffba692fd184a
Yu Fu <fybertas@gmail.com>
2019-11-04 13:31:46 -0500

    updated debugging setup and the document for partial inverse
    updated launch.json for debugging setup as well as adding the document describing how to get the partially inverse prolog program.

--- c-to-prolog/babel.pl
+++ c-to-prolog/babel.pl
@@ -1,13 +1,11 @@
-
-
 main(ARGC, ARGV, BABEL_RET) :- 
 
 
  J is 8,
 I is 65535,loop_entry_0(J01, J01),
-BABEL_RET is 0, true. 
+BABEL_RET is 0, true.
 
- 
+
 loop_entry_0(J01, FinalJ) :- J > 0-> !,BabelExp_0 is J - 1,
 J is BabelExp_0,
 loop_entry_0( J01 , FinalJ).
